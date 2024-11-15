6f170dc0bbda2d9c59c71850166f47fafd439358
ChainChomp5040 <156303681+ChainChomp5040@users.noreply.github.com>
2024-11-15 13:57:44 +1030

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 function calculate() {
-    console.log("Starting calculate"); 
     var num1 = document.getElementById("num1").value;
     var num2 = document.getElementById("num2").value;
     var ans = parseInt(num1) + parseInt(num2);
     document.getElementById("ans").value = ans;
-    console.log(ans); 
+}
+
+function calculateex() {
+    var num1ex = document.getElementById("num1ex").value;
+    var num2ex = document.getElementById("num2ex").value;
+    var ansex = Math.pow(parseInt(num1ex),parseInt(num2ex));
+    document.getElementById("ansex").value = ansex;
 }
